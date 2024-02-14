8f7a61f78f15a4b7f51d8577dd5a7f214cde1cbf
Gijysm <117034463+Gijysm@users.noreply.github.com>
2024-02-14 12:57:07 +0200

    #Добавлений інтерфейс State

--- Project14/Game.h
+++ Project14/Game.h
@@ -1,13 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Network.hpp>
-
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include "State.h"
 using namespace sf;
 using namespace std;
 
--- Project14/State.h
+++ Project14/State.h
@@ -1,5 +1,26 @@
-#pragma once
+#ifndef STATE_H
+#define STATE_H
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+
 class State
 {
-};
+private:
+	vector<Texture> texture;
 
+public:
+	State();
+	virtual ~State();
+
+	virtual void update() = 0;
+	virtual void render() = 0;
+};
+#endif
