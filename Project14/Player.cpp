75231109d6eabdb34fd8d2a66bce7419581aae58
Gijysm <117034463+Gijysm@users.noreply.github.com>
2024-02-21 22:05:52 +0200

    return anim

--- Project14/Player.cpp
+++ Project14/Player.cpp
@@ -16,7 +16,8 @@ Player::Player(float x, float y, map < string, Texture>& texture_sheet)
 	this->SetPosition(x, y);
 	this->CreateMovementComponent(300.f, 30.f, 5.f);
 	this->CreateAnimationComponent(texture_sheet);
-	this->animationComponent->addAnimation("Idle", 1.f, 0, 0, 2, 4, 128, 64);
+    this->animationComponent->addAnimation("Idle", 1.f, 0, 0, 2, 4, 128, 64);
+    this->animationComponent->addAnimation("Run", 1.f, 0, 0, 2, 4, 128, 64);
 }
 
 Player::~Player()
